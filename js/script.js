56ca6002dcef99b586f394bbbbedc8ea5c1a6405
Sharifpr <sharifpr532@gmail.com>
2021-09-02 10:20:58 +0600

    added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Global variable */
 const searchInput = document.getElementById('input-field');
 const searchButton = document.getElementById('search-button');
 const searchContainer = document.getElementById('search-container');
@@ -5,8 +6,9 @@ const errorMassage = document.getElementById('error-massage');
 
 searchButton.addEventListener('click', function () {
     const searchText = searchInput.value;
+
     if (searchText === "") {
-        errorMassage.innerText = `Please Carrect Input Value...`;
+        errorMassage.innerText = `Please Carrect Input Value...!`;
         return;
     }
 
@@ -17,23 +19,24 @@ searchButton.addEventListener('click', function () {
     fetch(url)
         .then(res => res.json())
         .then(data => searchBookDetails(data.docs))
+    // .finally(() => searchInput.value === "");
 });
 
 const searchBookDetails = books => {
-    // console.log(books)
+
+    /* error handling */
     if (books.length === 0) {
-        errorMassage.innerText = `No book found for ${searchInput.value} search result !`;
+        errorMassage.innerText = `No book found for "${searchInput.value}" search result !`;
     }
     else {
         errorMassage.innerText = "";
     }
 
     books.forEach(book => {
-        // console.log(book)
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card h-100">
+            <div class="card h-100 shadow">
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h2 class="card-title">Book Name: ${book.title}</h2>
