b07da62726249f3776eddf4c628f444c6f5ffc55
Sharifpr <sharifpr532@gmail.com>
2021-09-02 15:45:54 +0600

    added

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ searchButton.addEventListener('click', function () {
     // clear field
     searchContainer.textContent = "";
 
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => searchBookDetails(data.docs))
